87e5fd6703b693640028dcfb7c28b27e8f4467fb
Pedro Delfino <p.delfino01@gmail.com>
2019-10-04 19:40:21 -0300

    cap 6 files

--- cap-5/q-18.py
+++ cap-5/q-18.py
@@ -29,17 +29,22 @@ def crivo_eratostenes(n):
 # função para checar se é da forma 4n+1 ou 4n+3
 # n é o alcance
 
-def primos_congruos(a,r):
-    resultado=[]
-    primos=crivo_eratostenes(r)
-    for p in primos:
+def primos_congruentes(a,r):
+    
+    result = []
+    
+    num_primos = crivo_eratostenes(r)
+    
+    for p in num_primos:
+    
         if  (a**(p-1))%(p**2)==1:
-            resultado.append(p)
-    return resultado  
+            result.append(p)
+    
+    return result  
 
 print ("Fazendo os exemplos do livro. Com a=2,5,10,14 são 2 primos. E com a=19 são 5 primos, respectivamente: ")
-print (primos_congruos(2,100000))
-print (primos_congruos(5,100000))
-print (primos_congruos(10,100000))
-print (primos_congruos(14,100000))
-print (primos_congruos(19,100000))
+print (primos_congruentes(2,100000))
+print (primos_congruentes(5,100000))
+print (primos_congruentes(10,100000))
+print (primos_congruentes(14,100000))
+print (primos_congruentes(19,100000))
