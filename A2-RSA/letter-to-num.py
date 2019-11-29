b12816913d8b5d181938e0a57ecc6813fae8f3ff
Pedro Delfino <p.delfino01@gmail.com>
2019-11-29 15:16:27 -0300

    conversor

--- A2-RSA/letter-to-num.py
+++ A2-RSA/letter-to-num.py
@@ -1,6 +1,50 @@
 dict = {"a":10,"b":11,"c":12,"d":13,"e":14,"f":15,
         "g":16,"h":17,"i":18,"j":19,"k":20,"l":21,
-        "m":20,"n":21,"o":22,"p":23,"q":24,
-        "r":26,"s":27,"t":28,"u":29,"v":30,"w":31,"x":32,"y":33,"z":34}
+        "m":22,"n":23,"o":24,"p":25,"q":26,
+        "r":27,"s":28,"t":29,"u":30,"v":31,"w":32,"x":33,"y":34,"z":35," ":36}
 
 #duas funções
+#for i in dict:
+ #   print (i,dict[i])
+
+def char_to_num(string):
+
+    string = string.lower()  
+    
+    lista_num = []
+
+    for char in string:
+        #print (char) 
+        for i in dict:
+            #print (i)  
+            if char==i:
+                
+                lista_num.append(dict[i])
+                break
+    return lista_num
+
+#print (char_to_num("a"))
+teste= char_to_num("BRENO bumbum guloso")
+teste_bruna = char_to_num("matematica")
+print (teste_bruna)
+
+def num_to_char(lista_num):
+
+    lista_char = []
+
+    for num in lista_num:
+        for i in dict:
+            if num==dict[i]:
+
+                lista_char.append(i)
+                break
+
+    final_string = ""
+    
+    for i in lista_char:
+        final_string += str(i)
+
+    return final_string
+
+print (num_to_char(teste))
+print (num_to_char(teste_bruna))
