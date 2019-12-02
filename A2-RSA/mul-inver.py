d0dae7cd2f0d851b8cbe7abbedc6b0ad1ef63cba
Pedro Delfino <p.delfino01@gmail.com>
2019-12-02 09:35:59 -0300

    mul-inver

--- A2-RSA/mul-inver.py
+++ A2-RSA/mul-inver.py
@@ -1,21 +1,23 @@
-# bruna:
-def xgcd(a, b):
-    x0, x1, y0, y1 = 0, 1, 1, 0
-    
+
+def alg_euclides_est(a, b):
+    #retorna (x, y) tal que a*x + b*y = 1
+    d, x, y, z = 0, 1, 1, 0
     while a != 0:
         q, b, a = b // a, a, b % a
-        y0, y1 = y1, y0 - q * y1
-        x0, x1 = x1, x0 - q * x1
-    
-    return b, x0, y0
+        y, z = z, y - q * z
+        d, x = x, d - q * x
+    return b, d, y
+
+print (alg_euclides_est(7,14))
 
 # retorna x de modo que:    (x*a) === 1 (mod b)
 def mulinv(a, b):
-    g, x, _ = xgcd(a, b)
+    g, x, _ = alg_euclides_est(a, b)
     
     if g == 1:
         return x % b
     else:
         return "MDC("+str(a)+","+str(b)+")!=1, logo, não tem inversa"
 
-
+for i in range (100,10000):
+    print (mulinv(i,237))
